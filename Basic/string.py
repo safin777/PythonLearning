ddd960693aa45579977dd8ad9ff0e37c88fbf1be
safin777 <sayedislamsafin7770555@gmail.com>
2023-04-25 21:40:53 +0600

    Python -String

---
+++ Basic/string.py
@@ -0,0 +1,19 @@
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+#print(a)  
+
+#output:
+# Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.
+
+
+#####  Slicing Strings #####
+
+_slice_string = "Hello, World!"
+print(_slice_string[2:5]) 
+
+#output: llo
