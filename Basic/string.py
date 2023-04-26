bf54b02d7d8e3fc482d82ca6bf04a918d18e7bac
safin777 <sayedislamsafin7770555@gmail.com>
2023-04-26 13:03:22 +0600

    string new funtionality added

--- Basic/string.py
+++ Basic/string.py
@@ -2,7 +2,7 @@ a = """Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
-#print(a)  
+print(a)  
 
 #output:
 # Lorem ipsum dolor sit amet,
@@ -17,3 +17,16 @@ _slice_string = "Hello, World!"
 print(_slice_string[2:5]) 
 
 #output: llo
+
+######Slice To the End ######
+print ("Slice from index[2] to end:",_slice_string[2:])
+#output: llo, World!
+
+###### Negative Indexing ######
+print ("Negative Indexing:",_slice_string[-5:-2])
+#output: orl
+
+###### String Length ######
+# It will count the length of the string with white space
+print ("String Length:",len(_slice_string))
+#output: 13
